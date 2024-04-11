d2aac7c0629c817259907a0856cf64d35c4e1d34
Manas Mahale <manas.mahale2002@gmail.com>
2024-04-11 06:32:23 +0530

    benchmark added

---
+++ src/benchmark.py
@@ -0,0 +1,104 @@
+import os
+import numpy as np
+import pandas as pd
+import networkx as nx
+from tqdm import tqdm
+from loguru import logger
+from molecularnetwork import MolecularNetwork
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, balanced_accuracy_score, f1_score, precision_score, recall_score, roc_auc_score
+
+from tree import TopologicalDecisionTreeClassifier
+
+
+# Define a function to evaluate the models and store metrics
+def evaluate_models(dataset_name, X_train, y_train, A_train, X_test, y_test):
+    # Train TopologicalDecisionTreeClassifier
+    topo_clf = TopologicalDecisionTreeClassifier(max_depth=10)
+    logger.debug(f'Training {dataset_name} TopologicalDecisionTreeClassifier model')
+    topo_clf.fit(X_train, y_train, A_train)
+    logger.debug(f'Training Completed {dataset_name} TopologicalDecisionTreeClassifier model')
+
+    # Train DecisionTreeClassifier
+    clf = DecisionTreeClassifier()
+    clf.fit(X_train, y_train)
+
+    # Make predictions
+    topo_pred = topo_clf.predict(X_test)
+    pred = clf.predict(X_test)
+
+    # Calculate evaluation metrics
+    metrics = {
+        "Dataset": dataset_name,
+        "Topological_Decision_Tree_Accuracy": accuracy_score(y_test, topo_pred),
+        "Topological_Decision_Tree_Precision": precision_score(y_test, topo_pred),
+        "Topological_Decision_Tree_Recall": recall_score(y_test, topo_pred),
+        "Topological_Decision_Tree_Balanced_Accuracy": balanced_accuracy_score(y_test, topo_pred),
+        "Topological_Decision_Tree_F1_Score": f1_score(y_test, topo_pred),
+        "Topological_Decision_Tree_AUC_ROC": roc_auc_score(y_test, topo_pred),
+        "Decision_Tree_Accuracy": accuracy_score(y_test, pred),
+        "Decision_Tree_Precision": precision_score(y_test, pred),
+        "Decision_Tree_Recall": recall_score(y_test, pred),
+        "Decision_Tree_Balanced_Accuracy": balanced_accuracy_score(y_test, pred),
+        "Decision_Tree_F1_Score": f1_score(y_test, pred),
+        "Decision_Tree_AUC_ROC": roc_auc_score(y_test, pred)
+    }
+
+    return metrics
+
+def preprocess_data(df):
+
+    # train
+    train_df = df[df["split"] == "train"]
+    train_smiles_list = train_df["canonical_smiles"].values
+    train_classes = train_df["active"].values
+    network = MolecularNetwork(descriptor="morgan2", sim_metric="tanimoto", sim_threshold=0.5)
+    graph = network.create_graph(train_smiles_list, train_classes)
+    X_train = np.array([graph.nodes[i]['fp'] for i in graph.nodes])
+    y_train = np.array([int(graph.nodes[i]['categorical_label']) for i in graph.nodes])
+    A_train = nx.adjacency_matrix(graph, weight=None)
+
+    # test
+    test_df = df[df["split"] == "test"]
+    test_smiles_list = test_df["canonical_smiles"].values
+    test_classes = test_df["active"].values
+    test_network = MolecularNetwork(descriptor="morgan2", sim_metric="tanimoto", sim_threshold=0.5)
+    test_graph = test_network.create_graph(test_smiles_list, test_classes)
+    X_test = np.array([test_graph.nodes[i]['fp'] for i in test_graph.nodes])
+    y_test = np.array([int(test_graph.nodes[i]['categorical_label']) for i in test_graph.nodes])
+    
+    return (X_train, y_train, A_train, X_test, y_test)
+
+
+# Path to the folder containing datasets
+data_folder = '../data/'
+
+# Initialize an empty list to store metrics for all datasets
+all_metrics = []
+
+# Iterate over each dataset file in the data folder
+for file_name in tqdm(os.listdir(data_folder), leave=False, desc="Datasets Covered"):
+    if file_name.endswith('.csv'):
+        # Load dataset
+        df = pd.read_csv(os.path.join(data_folder, file_name))
+
+        logger.info(f'Loading {file_name}')
+        # Extract features and labels
+        (X_train, y_train, A_train, X_test, y_test) = preprocess_data(df)
+        logger.info(f'Processing {file_name[:-4]} Done')
+
+        # Evaluate models and store metrics
+        dataset_name = os.path.splitext(file_name)[0]
+
+        logger.info(f'Evaluating model for {file_name[:-4]}')
+        metrics = evaluate_models(dataset_name, X_train, y_train, A_train, X_test, y_test)
+        logger.info(f'Evaluation done for {file_name[:-4]}')
+
+        # Append metrics to the list
+        all_metrics.append(metrics)
+
+        # Convert the list of dictionaries to a DataFrame
+        metrics_df = pd.DataFrame(all_metrics)
+
+        # Save the metrics to a CSV file
+        metrics_df.to_csv('metrics_results.csv', index=False)
--- src/tree.py
+++ src/tree.py
@@ -1,13 +1,6 @@
 """Implementation of Decision Trees with Topological Impurity"""
 
 import numpy as np
-import pandas as pd
-import networkx as nx
-from loguru import logger
-from molecularnetwork import MolecularNetwork
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import classification_report
-
 
 class TopologicalDecisionTreeClassifier:
     def __init__(self, max_depth=None, min_samples_split=2, min_samples_leaf=1):
@@ -33,16 +26,19 @@ class TopologicalDecisionTreeClassifier:
 
         left_indices = X[:, best_split_feature] <= best_split_value
         right_indices = ~left_indices
-
+        print(f"{left_indices.sum() = }, {right_indices.sum() = }")
         left_tree = self._build_tree(X[left_indices], y[left_indices], adj_matrix[left_indices][:, left_indices], depth + 1)
         right_tree = self._build_tree(X[right_indices], y[right_indices], adj_matrix[right_indices][:, right_indices], depth + 1)
 
         return {'split_feature': best_split_feature, 'split_value': best_split_value, 'left': left_tree, 'right': right_tree}
 
     def _create_leaf_node(self, y):
-        unique_classes, counts = np.unique(y, return_counts=True)
-        majority_class = unique_classes[np.argmax(counts)]
-        return {'leaf': True, 'class': majority_class}
+        if y.any():
+            unique_classes, counts = np.unique(y, return_counts=True)
+            majority_class = unique_classes[np.argmax(counts)]
+            return {'leaf': True, 'class': majority_class}
+        else:
+            return {'leaf': True, 'class': 0} # FIXME ??
 
     def _topological_impurity(self, y, adj_matrix):
         """Calculate Topological Impurity"""
@@ -55,15 +51,20 @@ class TopologicalDecisionTreeClassifier:
         # Compute the product of class proportions
         class_proportions_product = np.prod(class_counts / total_samples)
         
-        # Compute the topological impurity
-        topological_impurity = class_proportions_product * (1 + edges_between_classes / np.sum(adj_matrix))
+        total_edges = np.sum(adj_matrix)
+        # Avoid ZeroDivisionError
+        if total_edges == 0:
+            return class_proportions_product
 
+        # Compute the topological impurity
+        topological_impurity = class_proportions_product * (1 + edges_between_classes / total_edges)
         return topological_impurity
 
     def _find_best_split(self, X, y, adj_matrix):
-        best_impurity = np.inf
+        best_impurity_reduction = -np.inf
         best_split_feature = None
         best_split_value = None
+        parent_impurity = self._topological_impurity(y, adj_matrix)
 
         for feature in range(X.shape[1]):
             unique_values = np.unique(X[:, feature])
@@ -75,8 +76,10 @@ class TopologicalDecisionTreeClassifier:
                 right_impurity = self._topological_impurity(y[right_indices], adj_matrix[right_indices][:, right_indices])
 
                 child_impurity = left_indices.sum() / len(X) * left_impurity + right_indices.sum() / len(X) * right_impurity
-                if child_impurity < best_impurity:
-                    best_impurity = child_impurity
+                impurity_reduction = parent_impurity - child_impurity # max this
+                
+                if impurity_reduction > best_impurity_reduction:
+                    best_impurity_reduction = impurity_reduction
                     best_split_feature = feature
                     best_split_value = split_value
 
@@ -88,6 +91,12 @@ class TopologicalDecisionTreeClassifier:
             predictions[i] = self._predict_sample(sample, self.tree_)
         return predictions
 
+    def predict_proba(self, X):
+        proba = np.zeros((len(X), self.n_classes_))
+        for i, sample in enumerate(X):
+            proba[i] = self._predict_proba_sample(sample, self.tree_)
+        return proba
+
     def _predict_sample(self, sample, node):
         if node.get('leaf'):
             return node['class']
@@ -97,45 +106,13 @@ class TopologicalDecisionTreeClassifier:
         else:
             return self._predict_sample(sample, node['right'])
 
-
-if __name__ == "__main__":
-    df = pd.read_csv('../data/CHEMBL1267247.csv')
-
-    # train
-    train_df = df[df["split"] == "train"]
-    train_smiles_list = train_df["canonical_smiles"].values
-    train_classes = train_df["active"].values
-    network = MolecularNetwork(descriptor="morgan2", sim_metric="tanimoto", sim_threshold=0.5)
-    graph = network.create_graph(train_smiles_list, train_classes)
-    X_train = np.array([graph.nodes[i]['fp'] for i in graph.nodes])
-    y_train = np.array([int(graph.nodes[i]['categorical_label']) for i in graph.nodes])
-    A_train = nx.adjacency_matrix(graph, weight=None)
-
-    # test
-    test_df = df[df["split"] == "test"]
-    test_smiles_list = test_df["canonical_smiles"].values
-    test_classes = test_df["active"].values
-    test_network = MolecularNetwork(descriptor="morgan2", sim_metric="tanimoto", sim_threshold=0.5)
-    test_graph = test_network.create_graph(test_smiles_list, test_classes)
-    X_test = np.array([test_graph.nodes[i]['fp'] for i in test_graph.nodes])
-    y_test = np.array([int(test_graph.nodes[i]['categorical_label']) for i in test_graph.nodes])
-    A_test = nx.adjacency_matrix(test_graph, weight=None)
-
-    # Train TopologicalDecisionTreeClassifier
-    topo_clf = TopologicalDecisionTreeClassifier()
-    topo_clf.fit(X_train, y_train, A_train)
-
-    # Train DecisionTreeClassifier
-    clf = DecisionTreeClassifier()
-    clf.fit(X_train, y_train)
-
-    # Make predictions
-    topo_pred = topo_clf.predict(X_test)
-    pred = clf.predict(X_test)
-
-    # Generate classification report
-    print("Topological Decision Tree Classifier:")
-    print(classification_report(y_test, topo_pred))
-
-    print("\n\nDecision Tree Classifier:")
-    print(classification_report(y_test, pred))
+    def _predict_proba_sample(self, sample, node):
+        if node.get('leaf'):
+            class_proba = np.zeros(self.n_classes_)
+            class_proba[node['class']] = 1.0
+            return class_proba
+        
+        if sample[node['split_feature']] <= node['split_value']:
+            return self._predict_proba_sample(sample, node['left'])
+        else:
+            return self._predict_proba_sample(sample, node['right'])
